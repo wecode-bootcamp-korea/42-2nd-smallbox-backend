4d7222f96c08071a2949a1c7cba4ea17e2964664
juseong <jjs7280@gmail.com>
2023-02-26 13:21:53 +0900

    [42기 장주성] [ADD] error handling 기능 추가

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
+const { globalErrorhandler } = require('./utils/error');
+
 const createApp = () => {
   const app = express();
 
   app.use(express.json());
   app.use(cors());
   app.use(morgan('tiny'));
+
+  app.all('*', (req, res, next) => {
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.statusCode = 404;
+    next(err);
+  });
+
+  app.use(globalErrorhandler);
 
   // health check
   app.get('/ping', (req, res) => {
